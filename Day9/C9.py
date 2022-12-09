6240c5e55534465674776733d8232c82a041b131
Javier Aguilar Martin <ja683@kent.ac.uk>
2022-12-09 16:05:12 +0000

    keep debugging

--- Day9/C9.py
+++ Day9/C9.py
@@ -4,9 +4,12 @@ from get_data import get_input
 text = get_input(9)
 import numpy as np
 
+
 motions = list(map(lambda x: (x.split(' ')[0], int(x.split(' ')[1])), text.splitlines()))
 print(motions[0])
-S = H = T = np.array([0,0])
+S = np.array([0,0])
+H = S.copy()
+T = S.copy()
 visited = {tuple(S)}
 visited_amount = 1
 
@@ -25,27 +28,42 @@ def straight_move(mark: np.ndarray(shape=(1,2)), direction: str, steps: int, cou
         movement = np.array([0,-1])
     if count:
         for i in range(1,steps+1):
-            mark += i*movement
+            mark += movement
             visited.add(tuple(mark)) # There is no need to store the visited positions, but I want to do it
     else:
         mark += steps*movement
     return mark, visited
+
+#print(straight_move(H, *motions[0], count = True, visited = visited))
     
 def diagonal_move(mark, horizontal, vertical, visited):
     mark += np.array([horizontal, vertical])
     visited.add(tuple(mark))
     return mark, visited
 
+#print(diagonal_move(H, 1,1, visited = visited))
+    
 def move(mark, mode, *args, **kwargs):
     if mode == "s":
         return straight_move(mark, *args, **kwargs)
     elif mode == "d":
         return diagonal_move(mark, *args)
+# =============================================================================
+# print(T)
+# ex1, _ = move(H, "s", *motions[0], count = True, visited = visited)
+# print(T)
+# ex2, _ = move(T, "d", -1,1,visited )
+# print(T)
+# print(ex1)
+# print(ex2)
+# =============================================================================
 
 def touching(head: np.ndarray(shape=(1,2)), tail: np.ndarray(shape=(1,2))) -> bool:
     pseudo_distance = np.absolute(head - tail).max()
     touching = pseudo_distance <= 1
     return touching
+
+#print(touching(ex1,ex2))
 
 def same_row_or_column(head, tail):
     if head[0] == tail[0]:
@@ -79,20 +97,20 @@ def visited_positions(head, tail, start, motions):
             else:
                 if head[0] == tail[0] + 1:
                     diff = head[1] - tail[1]
-                    horizontal = (diff - 1)/abs(diff-1)
+                    horizontal = (diff - 1)//abs(diff-1)
                     vertical = 1
                 elif head[0] == tail[0] - 1:
                     diff = head[1] - tail[1]
-                    horizontal = (diff - 1)/abs(diff-1)
+                    horizontal = (diff - 1)//abs(diff-1)
                     vertical = -1
                 elif head[1] == tail[1] + 1:
                     diff = head[0] - tail[0]
                     horizontal = 1
-                    vertical = (diff - 1)/abs(diff-1)
+                    vertical = (diff - 1)//abs(diff-1)
                 elif head[1] == tail[1] - 1:
                     diff = head[0] - tail[0]
-                    horizontal = - 1
-                    vertical = (diff - 1)/abs(diff-1)
+                    horizontal = -1
+                    vertical = (diff - 1)//abs(diff-1)
 
                 tail, visited = move(tail, "d", horizontal, vertical, visited)
                 steps, direction = same_row_or_column(head, tail)
--- get_data.py
+++ get_data.py
@@ -6,7 +6,7 @@ AOC_SESSION = config('AOC_SESSION')
 
 
 s = requests.Session()
-s.auth = ('JaviMaligno', '76321jav')
+
 def get_input(day):
     resp = s.get(f'https://adventofcode.com/2022/day/{day}/input', cookies={'session':AOC_SESSION})
     text = resp.text
