847388b802cf7c59ec46cef3dbedaab417b46986
Javi <javiecija96@gmail.com>
2022-12-19 15:59:36 +0100

    Challenge 18

--- Day18/C18.py
+++ Day18/C18.py
@@ -3,6 +3,7 @@ from operator import itemgetter
 import numpy as np
 import sys
 sys.path.append(r"C:\Users\javia\OneDrive\Escritorio\GitHub\AoC2022")
+sys.setrecursionlimit(100000)
 from get_data import get_input
 text = get_input(18)
 #print(text)
@@ -37,14 +38,111 @@ def compute_sides(text):
 
 
 total, center_dict = compute_sides(text)
+coordinates = center_dict.keys()
+def rock(coordinates):
+    rock_points = defaultdict(bool)
+    for k in coordinates:
+        rock_points[k] = True
+    return rock_points
+#print(rock(coordinates))
+# I add and substract 1 because I will use the exterior to detect the surface
+def get_extremes(coordinates):
+    max_x = max(coordinates)[0]+1
+    min_x = min(coordinates)[0]-1
+    max_y = max(coordinates, key = itemgetter(1))[1]+1
+    min_y = min(coordinates, key = itemgetter(1))[1]-1
+    max_z = max(coordinates, key = itemgetter(2))[2]+1
+    min_z = min(coordinates, key = itemgetter(2))[2]-1
+    extremes = (min_x, max_x, min_y, max_y, min_z, max_z)
+    return extremes
+def adjacents(point, extremes):
+    min_x, max_x, min_y, max_y, min_z, max_z = extremes
+    x,y,z = point
+    adjacents = []
+    if min_x < x:
+        adjacents.append((x-1,y,z))
+    if x < max_x:
+        adjacents.append((x+1,y,z))
+    if min_y < y:
+        adjacents.append((x,y-1, z))
+    if y < max_y:
+        adjacents.append((x,y+1,z))
+    if min_z < z:
+        adjacents.append((x,y,z-1))
+    if z < max_z:
+        adjacents.append((x,y,z+1))
+    return adjacents
 
-points = center_dict.keys()
 
-max_x = max(points)
-min_x = min(points)
-max_y = max(points, key = itemgetter(1))
-min_y = min(points, key = itemgetter(1))
-max_z = min(points, key = itemgetter(2))
-min_z = min(points, key = itemgetter(2))
+#print(adjacents(start))
+extremes = get_extremes(coordinates)
+def dfs(start, extremes, rock_points, sides = 0, discovered = defaultdict(bool)):
+    sides = sides
+    discovered = discovered
+    if rock_points[start]:
+        sides +=1
+    else:
+        discovered[start] = True
+    
+    adjacent_cubes = adjacents(start, extremes)
+    for cube in adjacent_cubes:
+        if rock_points[cube]:
+            sides += 1
+            continue
+        if not discovered[cube]:
+            sides += dfs(cube, rock_points, sides = sides, discovered= discovered) 
+    return sides
+
+#surface_sides = dfs(start, rock_points)
+#print(surface_sides)
+
+#print(max_x,max_y, max_z, min_x,min_y, min_z)
+discovered = defaultdict(bool)
+
+def original_dfs(start, extremes):
+    if not discovered[start]:
+        print(start)
+        discovered[start] = True
+        adjacent_cubes = adjacents(start, extremes)
+        for cube in adjacent_cubes:
+            if not discovered[cube]:
+                original_dfs(cube, extremes)
+    return discovered
+min_x, max_x, min_y, max_y, min_z, max_z = extremes
+start = (max_x, max_y, max_z)
+#print(original_dfs(start,extremes))
+rock_points = rock(coordinates)
+discover = 0
+def dev_dfs(start, extremes, rock_points, discover = 0):
+    if not discovered[start]:
+        discovered[start] = True if not rock_points[start] else False
+        discover = discover +1 if rock_points[start] else discover
+        print(start, discovered[start], discover)
+        adjacent_cubes = adjacents(start, extremes)
+        for cube in adjacent_cubes:
+            if not discovered[cube] and not rock_points[cube]:
+                dev_dfs(cube, extremes, rock_points, discover = discover)
+    return discovered
+#print(dev_dfs(start,extremes, rock_points))
+
+def non_rec_dfs(start, extremes, rock_points, stack = [], sides = 0):
+    stack.append(start)
+    while stack:
+        v =  stack.pop()
+        if not discovered[v]:
+            discovered[v] = True
+            adjacent_cubes = adjacents(v, extremes)
+            print(v, adjacent_cubes)
+            for cube in adjacent_cubes:
+                if rock_points[cube]:
+                    sides += 1
+                    print(cube, "SIDE", sides)
+                    continue
+                elif not discovered[cube]:
+                    stack.append(cube)
+                    
+    return sides
+non_rec_dfs(start, extremes, rock_points)
+
 
 
--- Day18/test.py
+++ Day18/test.py
@@ -1,6 +1,19 @@
-from C18 import cubes, compute_sides
+from C18 import cubes, compute_sides, defaultdict, dfs, original_dfs, get_extremes, non_rec_dfs
 with open(r"C:\Users\javia\OneDrive\Escritorio\GitHub\AoC2022\Day18\test.txt") as f: 
     text = f.read()
-    center_list = cubes(text)
-    print(compute_sides(center_list))
+    total, center_dict = compute_sides(text)
     
+    coordinates = center_dict.keys()
+    rock_points = defaultdict(bool)
+    for k in coordinates:
+        rock_points[k] = True
+    extremes = get_extremes(coordinates)
+    min_x, max_x, min_y, max_y, min_z, max_z = extremes
+    start = (max_x, max_y, max_z)
+    discovered = defaultdict(bool)
+    start = (max_x,max_y,max_z)
+    print((max_x-min_x)*(max_y-min_y)*(max_z-min_z)) 
+    #surface_sides = dfs(start, rock_points)
+    #print(surface_sides)
+    #print(original_dfs(start, extremes))
+    non_rec_dfs(start, extremes, rock_points)
