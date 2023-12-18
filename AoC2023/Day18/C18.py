172b155e754f4f02309cdcb2064ed2bbd3dd972f
JaviMaligno <javiecija96@gmail.com>
2023-12-18 10:26:41 +0000

    no day 18

--- AoC2023/Day18/C18.py
+++ AoC2023/Day18/C18.py
@@ -1,4 +1,7 @@
 import sys
-sys.path.append(r"AoC2023")
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from get_data import get_input
 text = get_input(18)
+
+#Another flooding algorithm that I'm not doing
